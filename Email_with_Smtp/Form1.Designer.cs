e07efdfb42d8145c2e61fd66a90bfd9ebad2ba72
Adam Letenyei <35657485+ProjectPannonia@users.noreply.github.com>
2022-10-21 20:40:16 +0200

    Add project files.

---
+++ Email_with_Smtp/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace Email_with_Smtp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtFromAddress = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPwd = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtToAddress = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtBody = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(68, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(184, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "From Address";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtFromAddress
+            // 
+            this.txtFromAddress.CharacterCasing = System.Windows.Forms.CharacterCasing.Lower;
+            this.txtFromAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtFromAddress.Location = new System.Drawing.Point(258, 79);
+            this.txtFromAddress.Name = "txtFromAddress";
+            this.txtFromAddress.Size = new System.Drawing.Size(303, 38);
+            this.txtFromAddress.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(579, 79);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(134, 31);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Password";
+            // 
+            // txtPwd
+            // 
+            this.txtPwd.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtPwd.Location = new System.Drawing.Point(719, 79);
+            this.txtPwd.Name = "txtPwd";
+            this.txtPwd.Size = new System.Drawing.Size(240, 38);
+            this.txtPwd.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(68, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(153, 31);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "To Address";
+            // 
+            // txtToAddress
+            // 
+            this.txtToAddress.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtToAddress.Location = new System.Drawing.Point(258, 148);
+            this.txtToAddress.Name = "txtToAddress";
+            this.txtToAddress.Size = new System.Drawing.Size(303, 38);
+            this.txtToAddress.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(68, 206);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 31);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Body";
+            // 
+            // txtBody
+            // 
+            this.txtBody.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.txtBody.Location = new System.Drawing.Point(258, 206);
+            this.txtBody.Name = "txtBody";
+            this.txtBody.Size = new System.Drawing.Size(701, 38);
+            this.txtBody.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(258, 296);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(234, 31);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "SendMail";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1002, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtBody);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtToAddress);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtPwd);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtFromAddress);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtFromAddress;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPwd;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtToAddress;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtBody;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Email_with_Smtp/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+using System.Windows.Forms;
+
+namespace Email_with_Smtp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string strFromAddress = txtFromAddress.Text;
+                string strToAddress = txtToAddress.Text;
+                string strpwd = txtPwd.Text;
+                string strBody = txtBody.Text;
+
+                var smtpClient = new SmtpClient("smtp-mail.outlook.com")
+                {
+                    Port = 587,
+                    Credentials = new NetworkCredential(strFromAddress, strpwd),
+                    EnableSsl = true,
+
+                };
+
+                smtpClient.Send(strFromAddress,strToAddress, "This is SMTP Demo", strBody);
+            }catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+
+
+        }
+    }
+}
